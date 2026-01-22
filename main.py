2078ff20804e806746df03278fe5b1f006f8128d
AndrejBohus <131518484+AndrejBohus@users.noreply.github.com>
2026-01-22 17:19:18 +0000

    Problem somewhere

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 TICKER = "SPY"
 FAST = 50
 SLOW = 200
-LOOKBACK = 1000 
+LOOKBACK = 10000
 
 
 def get_data():
@@ -16,13 +16,37 @@ def get_data():
 
     return df.iloc[-LOOKBACK:, :]
 
-# def moving_averages(df, fast, slow):
-if __name__ == "__main__":
-    data = get_data()
+def moving_averages(df, fast, slow):
+    df[f"{FAST}_ma"] = df["Close"].rolling(fast).mean
+    df[f"{SLOW}_ma"] = df["Close"].rolling(slow).mean
 
-    #Catch blok
-    if data is None or data.empty:
-        print("Data wasnt loaded. Check TICKER")
-        exit() 
+    plt.plot(df["Close"])
+    plt.plot(df[f"{FAST}_ma"])
+    plt.plot(df[f"{SLOW}_ma"])
+    plt.legend(['Close', f'{FAST}_ma', f'{SLOW}_ma'])
+    plt.title("Moving Average Crossover")
+    return df.dropna()
 
-    print(f"Data for {TICKER} loaded ({len(data)} records).")
+def strategy(df, fast, slow):
+    # long když FAST > SLOW, short
+    df["Strategy"] = np.where(df[f"{fast}_ma"] > df[f"{slow}_ma"], 1, -1)
+    df["Strategy"] = df["Strategy"].shift(1)
+    return df
+
+def test_strategy(df, ticker, fast,slow):
+    df["Asset_Returns"] = (1 + df["Close"].pct_change()).cumprod() - 1
+    df["Strategy_Returns"] = (1 + df["Close"].pct_change() * df["Strategy"]).cumprod() - 1
+
+    plt.figure()
+    plt.plot(df["Asset_Returns"])
+    plt.plot(df["Strategy_Returns"])
+    plt.legend(f'{ticker} Cumulative Returns', f'{fast}- {slow} Crossover strategy returns')
+    plt.title("Moving Average Crossovers")
+    return df.dropna()
+
+df = get_data()
+df = moving_averages(df, FAST, SLOW)
+df = strategy(df, FAST, SLOW)
+df = test_strategy(df, TICKER, FAST,SLOW)
+
+df
